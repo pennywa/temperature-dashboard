4cebc0cae2f33e0f53248db08928faba02130ca4
Penny Wang <penny.ny.w@gmail.com>
2025-09-06 17:28:43 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,18 @@
+#######################
+# Import libraries
+import streamlit as st
+import pandas as pd
+import altair as alt
+import plotly.express as px
 
+#######################
+# Page configuration
+st.set_page_config(
+    page_title="Average Temperature of Countries from 1901 - 2022",
+    page_icon="🌐",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+alt.themes.enable("dark")
+
+#######################
